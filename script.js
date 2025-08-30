c0c26f29a497538ba4712016bcf2457a924e8928
gloatingcorner <bsbinc.gb@gmail.com>
2025-08-30 21:15:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,12 +20,5 @@ window.addEventListener("scroll", () => {
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("menu-toggle");
-  const menu = document.getElementById("menu");
 
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-  });
-});
 
